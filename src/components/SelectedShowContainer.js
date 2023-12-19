7ea5c1126002eb1a77ac90d0e5618e6443f2e44f
abdalla-wasaa <abdallawasaa7@gmail.com>
2023-12-19 10:21:55 +0300

    Attempted

--- src/components/SelectedShowContainer.js
+++ src/components/SelectedShowContainer.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import Episode from "./Components/Episode";
+import Episode from "./Episode";
 
 function SelectedShowContainer(props) {
-  const selectedSeason = useState(1);
+  let selectedSeason = useState(1);
 
   function mapSeasons() {
-    if (!!props.episodes) {
+    if (!props.episodes) {
       let seasons = props.episodes.map((e) => e.season).unique();
 
       return seasons.map((s) => {
@@ -20,7 +20,7 @@ function SelectedShowContainer(props) {
 
   function mapEpisodes() {
     return props.episodes.map((e) => {
-      if (e.season == selectedSeason) {
+      if (e.season === selectedSeason) {
         return <Episode eachEpisode={e} key={e.id} />;
       }
     });
@@ -48,8 +48,6 @@ function SelectedShowContainer(props) {
   );
 }
 
-export SelectedShowContainer;
-
 Array.prototype.unique = function () {
   const arr = [];
   for (let i = 0; i < this.length; i++) {
@@ -59,3 +57,5 @@ Array.prototype.unique = function () {
   }
   return arr;
 };
+
+export default SelectedShowContainer;
